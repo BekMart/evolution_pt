7390beaf2ebbf4c3adab38e2dcf2e5b0379b2a1a
BekMart <bekmart1989@gmail.com>
2025-09-17 14:36:07 +0100

    Connect to db/create superuser

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_pbLYAVE12NvW@ep-late-water-ab8ilt8b-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
