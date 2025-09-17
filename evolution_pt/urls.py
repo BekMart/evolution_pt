cb1e4f7d16458e62d72e9be8d59b2846b9ad188e
BekMart <bekmart1989@gmail.com>
2025-09-17 12:27:20 +0100

    Link homepage and test

--- evolution_pt/urls.py
+++ evolution_pt/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from home import views as home_views
 
 urlpatterns = [
+    path('', home_views.homepage, name='homepage'),
     path('admin/', admin.site.urls),
 ]
